aab97363fe20d99a35a0da71600fb2d12921ae3b
O Ohio <tekzo4real@gmail.com>
2020-05-30 03:21:28 -0400

    Update main.tf
    Update with Instance
    -Linux
    -Windows

--- main.tf
+++ main.tf
@@ -163,6 +163,11 @@ resource "aws_security_group" "test-private-sg" {
   }
 }
 
+#RDS Security Group
+resource "aws_security_group" "test-rds-sg" {
+  name= "sg_rds"
+  description = "Used for DB instances"
+  vpc_id      = aws_vpc.test-vpc.id
 # SQL access from public/private security group
 
 ingress {
@@ -174,3 +179,32 @@ ingress {
 }
 
 #COMPUTE
+#EC2 Instance
+## Linux
+
+resource "aws_instance" "test" {
+  instance_type                = "t2.micro"
+  ami                          = "ami-0323c3dd2da7fb37d"
+  key_name                     = "fire97"
+  subnet_id                    = aws_subnet.test-public.id
+  monitoring                   = true
+  associate_public_ip_address  = true
+
+  tags = {
+    Name = "test linux"
+  }
+}
+
+## Windows
+resource "aws_instance" "test2" {
+  instance_type                = "t2.micro"
+  ami                          = "ami-09d496c26aa745869"
+  key_name                     = "fire97"
+  subnet_id                    = aws_subnet.test-public.id
+  monitoring                   = true
+  associate_public_ip_address  = true
+
+  tags = {
+    Name = "test windows"
+  }
+}
