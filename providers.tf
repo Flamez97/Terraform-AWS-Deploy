2550d6a275d1f952ec7f29c8af5ffb7984597b13
O Ohio <tekzo4real@gmail.com>
2024-03-25 14:56:10 -0400

    update

--- providers.tf
+++ providers.tf
@@ -2,8 +2,3 @@ provider "aws" {
   region = "us-east-1"
   alias  = "east"
 }
-
-provider "aws" {
-  region = "us-east-2"
-  alias  = "east2"
-}
--- versions.tf
+++ versions.tf
@@ -1,8 +1,5 @@
 #Versions to be used
 
 terraform {
-  required_version = ">= 0.14.11"
-  required_providers {
-    aws = "~> 3.74.0"
-  }
+  required_version = ">= 1.7.0"
 }
